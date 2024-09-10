baf701fe8d8ab2d7f7600a9b31ea58d68492f3e3
Sebastian Carl <mrcaseb@gmail.com>
2024-09-10 10:00:13 +0200

    implement json formatting

--- R/season_and_week_utils.R
+++ R/season_and_week_utils.R
@@ -7,12 +7,15 @@
 #' If `FALSE`, will return current year on or after Thursday following Labor Day,
 #' i.e. Thursday after the first Monday in September. Otherwise previous year.
 #'
+#' @param as_json If `TRUE`, the function will output in JSON format. Defaults
+#'  to TRUE in non-interactive sessions.
+#'
 #' @return most recent season (a four digit numeric)
 #' @export
 #' @examples
 #' get_current_season()
 #' get_current_season(roster = TRUE)
-get_current_season <- function(roster = FALSE) {
+get_current_season <- function(roster = FALSE, as_json = !interactive()) {
   today <- Sys.Date()
   current_year <- as.integer(format(today, format = "%Y"))
   current_month <- as.integer(format(today, format = "%m"))
@@ -26,10 +29,10 @@ get_current_season <- function(roster = FALSE) {
       (isTRUE(roster) && current_month == 3 && current_day >= 15) ||
       (isTRUE(roster) && current_month > 3 )) {
 
-    return(current_year)
+    return(format_output(current_year, as_json = as_json))
   }
 
-  current_year - 1
+  format_output(current_year - 1, as_json = as_json)
 }
 
 #' Get Current Week
@@ -40,12 +43,12 @@ get_current_season <- function(roster = FALSE) {
 #'
 #' @examples
 #' get_current_week()
-#'
+#' @inheritParams get_current_season
 #' @return current nfl regular season week as a numeric
 #' @export
 #' @examples
 #' get_current_week()
-get_current_week <- function() {
+get_current_week <- function(as_json = !interactive()) {
 
   # Find first Monday of September in current season
   labor_day <- get_labor_day(get_current_season())
@@ -60,7 +63,7 @@ get_current_week <- function() {
   if(current_week < 1) current_week <- 1
   if(current_week > 22) current_week <- 22
 
-  current_week
+  format_output(current_week, as_json = as_json)
 }
 
 #' Compute Date of Labor Day
@@ -68,13 +71,13 @@ get_current_week <- function() {
 #' Computes the date of the Labor Day, i.e. the first Monday in September, in a given year.
 #'
 #' @param year Numeric or Character year. 4 Digits.
-#'
+#' @inheritParams get_current_season
 #' @export
 #' @return Object of Class `Date`
 #' @examples
 #' # 2023 Labor Day was on Sep 4th
 #' compute_labor_day(2023)
-get_labor_day <- function(year){
+get_labor_day <- function(year, as_json = !interactive()){
   stopifnot(length(year) == 1)
   earliest <- as.Date(paste(year, "09", "01", sep = "-"))
   latest <- as.Date(paste(year, "09", "08", sep = "-"))
@@ -91,10 +94,26 @@ get_labor_day <- function(year){
 #'
 #' @param start Numeric or Character year. 4 Digits.
 #' @param end Numeric or Character year. 4 Digits. Defaults to [get_current_season()]
-#'
+#' @inheritParams get_current_season
 #' @export
 #' @return Object of Class `integer`
 #' @examples
 #' # Compute range from 2016 to current season
 #' get_season_range(2016)
-get_season_range <- function(start = 1999, end = get_current_season()) seq(start, end)
+#' get_season_range(2016, as_json = TRUE)
+get_season_range <- function(start = 1999,
+                             end = get_current_season(),
+                             as_json = !interactive()){
+  format_output(seq(start, end), as_json = as_json)
+}
+
+format_output <- function(seasons, as_json) {
+  if (isFALSE(as_json)) return(seasons)
+  out <- paste0(
+    "[",
+    paste0(seasons, collapse = ","),
+    "]"
+  )
+  cat(out)
+}
+
